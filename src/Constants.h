e48b00a2722f91df8c7047e8ecc17c62c67131b5
Egor Kostiuk <kostukegor729@gmail.com>
2025-03-10 22:59:39 +0200

    update: main consts

--- src/Constants.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-constexpr unsigned int WINDOW_HEIGHT = 720;
-constexpr unsigned int WINDOW_WIDTH  = 1280;
-
---
+++ src/constants/MainConstants.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// Window size
+constexpr unsigned int WINDOW_HEIGHT = 720;
+constexpr unsigned int WINDOW_WIDTH  = 1280;
+
+// Maze scale
+const int cellSize = 20;
+
+// Maze size
+const int rows = 30;
+const int cols = 30;
