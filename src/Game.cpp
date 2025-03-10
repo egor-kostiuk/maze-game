9daa28f76ad5c099a5a822c20796fe2a81229c61
Egor Kostiuk <kostukegor729@gmail.com>
2025-03-10 23:04:19 +0200

    update: Game cpp

---
+++ src/Game.cpp
@@ -0,0 +1,44 @@
+#include <SFML/Graphics.hpp>
+#include "constants/MainConstants.h"
+#include "components/Maze.cpp"
+
+int main()
+{
+  // Create the main window
+  sf::RenderWindow window(sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}), "SFML window");
+
+  // Generate a maze
+  MazeGenerator maze(rows, cols);
+  maze.generateMaze();
+  vector<vector<int>> generatedMaze = maze.getMaze();
+
+  // Start the game loop
+  while (window.isOpen())
+  {
+    // Process events
+    while (const std::optional event = window.pollEvent())
+    {
+      // Close window: exit
+      if (event->is<sf::Event::Closed>())
+        window.close();
+    }
+
+    // Clear screen
+    window.clear(sf::Color::White);
+
+    // Drawing a maze
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        if (generatedMaze[i][j] == 1) {
+          sf::RectangleShape wall(sf::Vector2f(cellSize, cellSize));
+          wall.setFillColor(sf::Color::Black);
+          wall.setPosition(sf::Vector2f(j * cellSize, i * cellSize));
+          window.draw(wall);
+        }
+      }
+    }
+
+    // Update the window
+    window.display();
+  }
+}
